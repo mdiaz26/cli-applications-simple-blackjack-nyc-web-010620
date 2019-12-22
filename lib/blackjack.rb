e74b2896c69e380af3f0937026b22b644b0c83f8
Michael Diaz <mike.diaz006@gmail.com>
2019-12-22 22:48:50 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,58 @@
+require "pry"
+
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  card1 = deal_card
+  card2 = deal_card
+  card_total = card1 + card2
+  display_card_total(card_total)
+  return card_total
 end
 
-def hit?
-  # code hit? here
+def hit?(number)
+  prompt_user
+  input = get_user_input
+  # return number
+  if input == "s"
+    return number
+  elsif input == "h"
+    new_card = deal_card
+    new_card
+    display_card_total(number + new_card)
+    return (number + new_card)
+  else
+    invalid_command
+    prompt_user
+    get_user_input
+  # binding.pry
+  end
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
@@ -39,6 +60,14 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome 
+  card_total = initial_round
+  new_total = hit?(card_total)
+  if new_total > 21
+    end_game(new_total)
+  # binding.pry
+  else
+    hit?(new_total)
+  end
 end
     
--- lib/runner.rb
+++ lib/runner.rb
@@ -1,3 +1,7 @@
 require_relative "blackjack.rb"
+expect($stdout).to 
+receive (:puts).with("Type 'h' to hit or 's' to stay")
 
+expect(self).to
+receive(:get_user_input).and_return("s")
 runner
